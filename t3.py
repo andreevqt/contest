ba2d368117f8436adfa686c8c28c054ab21dd022
Alex <qprquo@gmail.com>
2022-05-06 14:25:24 +0300

    updates

--- t3.py
+++ t3.py
@@ -1,14 +1,13 @@
 from edx_io import edx_io
 
+
 def insertion_sort(arr):
     res = list(range(1, len(arr) + 1))
     for i in range(1, len(arr)):
         j = i - 1
         while j >= 0 and arr[j + 1] < arr[j]:
-            tmp = arr[j + 1]
-            arr[j + 1] = arr[j]
-            arr[j] = tmp
-            j = j - 1
+          arr[j], arr[j + 1] = arr[j + 1], arr[j]
+          j = j - 1
         res[i] = j + 2
     return res
 
